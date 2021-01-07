0148f944898a9a81feb820087518058729f9cd5f
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-01-07 17:47:27 +0100

    1st issue TODO : fermer la modale done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// si le header a la class topnav seulement on ajoute la class responsiv sinon on l'enleve
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -11,15 +12,19 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close"); // select btn closing modal
 
-// launch modal event
+// launch modal event 
+// tous les .modal-btn au click transforme le style display du modale en block
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-console.log("test");
-
-
+// au clic change le display de modalbg en none
+close.addEventListener("click", function(){
+  modalbg.style.display = "none";
+})
