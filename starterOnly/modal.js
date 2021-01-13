fbecbe9a3ece6d7ea5b797d67fe286f506eda5ef
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-01-13 13:44:00 +0100

    Issue 2 - (1) working on

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,19 +29,51 @@ close.addEventListener("click", function(){
   modalbg.style.display = "none";
 })
 
+
+// ISSUE 2 Implémenter entrées du formulaire
+//(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+const inputFirstName = document.getElementById("first")
+inputFirstName.addEventListener("input", function(e){
+  var value = e.target.value;
+ /* if(value == "a") {
+    alert("ça marche")
+  } else {
+    alert("ça ne marche pas")
+  }
+});*/
+
+
+//let nameInput = document.reserve.first.value
+// On vérifie (une fois que l'utilisateur a fini de saisir son prénom) ce qu'il a écrit 
+// si la valeur NE contient PAS au moins 2 caractères
+// alors le bouton submit du formulaire est disabled="true" 
+
+
+// ISSUE 3 Ajouter validation ou messages d'erreur
 /*
-(1) Lier les labels aux entrées dans le HTML en utilisant les attributs "for" et 
-"id" dans le code existant. 
-Corriger le code HTML quand nécessaire.
+Des messages d'erreur spécifiques doivent apparaître sous l'entrée qui n'est pas correcte.
+ Les messages d'erreur doivent s'afficher sous le champ de saisie associé. Exemples :
 
-Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
-Les données doivent être saisies correctement :
-(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
-(2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
-(3) L'adresse électronique est valide.
-(4) Pour le nombre de concours, une valeur numérique est saisie.
-(5) Un bouton radio est sélectionné.
-(6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
-Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
-
+"Veuillez entrer 2 caractères ou plus pour le champ du nom."
+"Vous devez choisir une option."
+"Vous devez vérifier que vous acceptez les termes et conditions."
+"Vous devez entrer votre date de naissance."
 */
+
+//ISSUE 4 Ajouter confirmation quand envoie réussi
+/*
+Après une validation réussie, inclure un message de confirmation 
+de la soumission réussie pour l'utilisateur 
+(ex. "Merci ! Votre réservation a été reçue.")
+*/
+
+//ISSUE 5 Tests manuels
+/*
+Visualiser et tester l'interface utilisateur dans les dernières versions de Chrome et de Firefox, 
+ainsi que dans les versions mobile et desktop. 
+Corriger les erreurs d'affichage existantes.
+Tester toutes les fonctionnalités des boutons et des entrées de formulaire 
+(tester les valeurs correctes et incorrectes)
+*/
+
+
