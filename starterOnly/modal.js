0139eaad9e0146f640610a129a9aa6e05670bca0
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-01-18 15:48:35 +0100

    added some com

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,4 +1,4 @@
-// si le header a la class topnav seulement on ajoute la class responsiv sinon on l'enleve
+//Only if header has .topnav class we add .responsive class otherwise we remove others class (add exclusively .topnav)
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -16,29 +16,27 @@ const close = document.querySelector(".close"); // select btn closing modal
 const inputFirstName = document.getElementById("first") // select first name in form input
 const inputLastName = document.getElementById("last") // last name
 const inputQuantity = document.getElementById("quantity") // last name
-
+const inputMail = document.getElementById('email') // mail
 // launch modal event 
-// tous les .modal-btn au click transforme le style display du modale en block
+// All .modal-btn become display block on click
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// au clic change le display de modalbg en none
+// On clic change modalbg display to "none"
 close.addEventListener("click", function(){
   modalbg.style.display = "none";
 })
 
+/*ISSUE 2 Implémenter entrées du formulaire*/
+//(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
 
-/*ISSUE 2 Implémenter entrées du formulaire
-(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
-(2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide. */
-inputFirstName.addEventListener("input", function(e){
-  var value = e.target.value;
-  inputFirstName.onblur = isFirstCorrect
+inputFirstName.addEventListener("input", function(e){ //listen if #first's input take an user input
+  var value = e.target.value; //get the value of the object that sent the event
+  inputFirstName.onblur = isFirstCorrect  //When inputFirstName lose focus => call the function
   function isFirstCorrect(){
     if (value.length < 2) {
       inputFirstName.classList.add("border-wrong");
@@ -50,8 +48,11 @@ inputFirstName.addEventListener("input", function(e){
   }
 });
 
+//(2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide. 
+
+// exactly the same as with first name
 inputLastName.addEventListener("input", function(e){
-  var value = e.target.value;
+  var value = e.target.value; 
   inputLastName.onblur = isFirstCorrect
   function isFirstCorrect(){
     if (value.length < 2) {
@@ -63,15 +64,14 @@ inputLastName.addEventListener("input", function(e){
   }
 });
 
-
 // (3) L'adresse électronique est valide.
-// Pour respecter la spécification HTML5 - issu de developper.mozzila
-var emailRegExp  = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-const inputMail = document.getElementById('email')
-inputMail.addEventListener("input", function(e){
-  var regex = inputMail.value.length === 0 || emailRegExp.test(inputMail.value);
-  if(regex) {
+// To respect the HTML5 specification - from developper.mozilla
+var emailRegExp  = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; //The list of mandatory characters : x@x
+
+inputMail.addEventListener("input", function(){
+  let goodMail = emailRegExp.test(inputMail.value);// compare regex with  the input and retourn boolean
+  if(goodMail) { //if true 
     inputMail.classList.add("border-good")
   } else {
     inputMail.classList.add("border-wrong");
@@ -79,15 +79,8 @@ inputMail.addEventListener("input", function(e){
   }
 })
 
-
-
-
-
-
-
-
-
 // (4) Pour le nombre de concours, une valeur numérique est saisie.
+
 inputQuantity.addEventListener("input", function(e){
   var value = e.target.value;
   inputQuantity.onblur = isItANum
@@ -103,6 +96,7 @@ inputQuantity.addEventListener("input", function(e){
 
 
 // (5) Un bouton radio est sélectionné.
+
 const inputRadio = document.querySelector('input[name="location"]:checked')
 // ou
 const inputRadio2 = reserve.radio['prenom']
@@ -118,6 +112,7 @@ for (var i = 0; i < radios.length; i++) {
 
 
 // (6) La case des conditions générales est cochée.
+
 const inputCheckbox = document.querySelector('checkbox1')
 
 
@@ -127,13 +122,21 @@ const inputCheckbox = document.querySelector('checkbox1')
 
 
 // ISSUE 3 Ajouter validation ou messages d'erreur
-/*
-Des messages d'erreur spécifiques doivent apparaître sous l'entrée qui n'est pas correcte.
-"Veuillez entrer 2 caractères ou plus pour le champ du nom."
-"Vous devez choisir une option."
-"Vous devez vérifier que vous acceptez les termes et conditions."
-"Vous devez entrer votre date de naissance."
-*/
+//Des messages d'erreur spécifiques doivent apparaître sous l'entrée qui n'est pas correcte.
+
+
+//"Vous devez choisir une option."
+
+
+//"Vous devez vérifier que vous acceptez les termes et conditions."
+
+
+
+//"Vous devez entrer votre date de naissance."
+
+
+
+
 
 //ISSUE 4 Ajouter confirmation quand envoie réussi
 /*
